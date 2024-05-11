60096205f2c5e822f1ed463b0e6757d10074946b
518651 <1725212799@qq.com>
2024-05-11 12:01:51 +0800

    侧边栏布局修改

--- src/components/Sidepanel.tsx
+++ src/components/Sidepanel.tsx
@@ -6,7 +6,7 @@ export default function Sidepanel() {
   return (
     <div>
       {/* PC桌面加载 */}
-      <aside className="min-w-60 relative w-full flex-col text-sm hidden bg-zinc-50 p-3 lg:flex lg:flex-col lg:border-r lg:w-60 xl:w-72">
+      <aside className="min-w-60 h-screen relative w-full flex-col text-sm hidden bg-zinc-50 p-3 lg:flex lg:flex-col lg:border-r lg:w-60 xl:w-72">
         <NavContent />
       </aside>
       {/* 移动设备加载 */}
